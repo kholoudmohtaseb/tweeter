848ff9ceed92134c0c4ab3633fbdc741b0a782b0
RamiNawahda <rami.nawahda@gmail.com>
2021-01-30 18:26:46 +0200

    test some routes and start connecting

--- client/src/components/Tweet/Tweet.js
+++ client/src/components/Tweet/Tweet.js
@@ -1,33 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import pic from "./avatar.png";
+import tweets from "../../apis/tweets";
 
 const Tweet = (props) => {
+  const [tweetInfo, setTweetInfo] = useState([]);
+
+  const fetchTweets = async () => {
+    const response = await tweets.get("/gitalltweete/");
+    console.log("-------------------", response.data);
+    setTweetInfo(response.data);
+  };
+  useEffect(() => {
+    fetchTweets();
+  }, []);
   return (
-    <div class="ui card" style={{ width: "600px", height: '300px', padding: '-3px' }}>
+    <div
+      class="ui card"
+      style={{ width: "600px", height: "300px", padding: "-3px" }}
+    >
       <div class="content">
         <img class="ui avatar image" src={pic} alt="user img" />
-          Elliot
-          <div class="meta">14h</div>
+        {tweetInfo.username}
+        <div class="meta">{tweetInfo.createdAt}</div>
       </div>
-      <div class="description">
-        Elyse is a copywriter working in New York.
-        </div>
-      <div class="image" >
-        <img src={pic} alt="tweet img" style={{ width: "450px", height: '100px', padding: '-1px' }} />
+      <div class="description">{tweetInfo.description}</div>
+      <div class="image">
+        <img
+          src={pic}
+          alt="tweet img"
+          style={{ width: "450px", height: "100px", padding: "-1px" }}
+        />
       </div>
 
-      <div class="ui horizontal segments" style={{ width: "600px", height: '100px' }}>
-        <div class="ui segment" style={{ width: "50px", height: '50px', padding: '5px' }} >
-          <i class="comment icon">comment</i>
+      <div
+        class="ui horizontal segments"
+        style={{ width: "600px", height: "100px" }}
+      >
+        <div
+          class="ui segment"
+          style={{ width: "50px", height: "50px", padding: "5px" }}
+        >
+          <i class="comment icon">{props.comments}</i>
         </div>
         <div class="ui segment">
-          <i class="retweet icon" >retweet</i>
+          <i class="retweet icon">retweet</i>
         </div>
         <div class="ui segment">
-          <i class="heart outline like icon" >liked</i>
+          <i class="heart outline like icon">liked</i>
         </div>
         <div class="ui segment">
-          <i class="bookmark outline icon" >saved</i>
+          <i class="bookmark outline icon">saved</i>
         </div>
       </div>
 
